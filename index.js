27f8deb12f7a8e2f8090c75037f5e89dbb45a4c8
komed3 <pk97@outlook.de>
2024-12-29 16:10:46 +0100

    setBounds + setTickCount methods

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
+/**
+ * linscale
+ * lightweight npm package to create linear scales
+ * 
+ * @author komed3 (Paul Köhler)
+ * @version 1.0.0
+ * @license MIT
+ */
+
 'use strict';
+
+module.exports = class LinScale {
+
+    constructor ( _low, _high, _ticks ) {
+
+        if ( _low && _high ) {
+
+            this.setBounds( _low, _high );
+
+        }
+
+        if ( _ticks ) {
+
+            this.setTickCount( _ticks );
+
+        }
+
+    };
+
+    setBounds ( low, high ) {
+
+        low = parseFloat( low );
+        high = parseFloat( high );
+
+        this.lowerBound = Math.min( low, high );
+        this.upperBound = Math.max( low, high );
+
+    };
+
+    setTickCount ( ticks ) {
+
+        this.maxTicks = parseInt( ticks );
+
+    };
+
+};
